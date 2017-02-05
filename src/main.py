10fdc80bf974935f77cdfa5aabc1f51418000322
overfreedom <1449429537@qq.com>
2017-02-05 23:40:02 +0800

    20170205

--- src/main.py
+++ src/main.py
@@ -5,9 +5,9 @@ if __name__ == '__main__':
     from urllib.request import urlopen
     from bs4 import BeautifulSoup
     html = urlopen(r'http://baidu.com')
-    print(html.read())
-#     bs = BeautifulSoup(html.read(),'html.parser')
-#     print(bs)
+#     print(html.read())
+    bs = BeautifulSoup(html.read(),'html.parser')
+    print(bs)
 #     imageLocation = bs.find('link',{'rel':'shortcut icon'})['href']
 #     print(imageLocation)
     
