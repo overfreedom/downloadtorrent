e7ac75942d2f8c78a471551b7a4550fcd2107299
overfreedom <1449429537@qq.com>
2017-02-06 18:06:32 +0800

    20170206

--- src/main.py
+++ src/main.py
@@ -1,15 +1,29 @@
-
+#encoding=utf-8
 if __name__ == '__main__':
-    print('in main')
-    from urllib.request import urlretrieve 
-    from urllib.request import urlopen
+#     print('in main')
+#     from urllib.request import urlretrieve 
+#     from urllib.request import urlopen
+#     import urllib.request
+#     from bs4 import BeautifulSoup
+#     url=r'http://www.baidu.com'
+# #     html = urlopen(url)
+# #     print(html.read())
+# #     bs = BeautifulSoup(html,'html.parser')
+# #     print(bs)
+# #     imageLocation = bs.find('link',{'rel':'shortcut icon'})['href']
+# #     print(imageLocation)
+# #     urlretrieve(url+imageLocation,'cc.ico')
+#     proxy ={'http:':'42.159.251.84:41795'}
+#     p = urllib.request.ProxyHandler(proxy)
+#     opener = urllib.request.build_opener(p)
+#     urllib.request.install_opener(opener)
+#     html =urllib.request.urlopen(url,timeout=500)
+#     bs = BeautifulSoup(html,'html.parser')
+#     print(bs)
+
+    import requests
     from bs4 import BeautifulSoup
-    html = urlopen(r'http://baidu.com')
-#     print(html.read())
-    bs = BeautifulSoup(html.read(),'html.parser')
-    print(bs)
-#     imageLocation = bs.find('link',{'rel':'shortcut icon'})['href']
-#     print(imageLocation)
-    
-    
-    
+    r= requests.get('http://www.baidu.com')
+    r.encoding='utf-8'
+    bs =BeautifulSoup(r.text,'html.parser') 
+    print(bs.find('title'))
